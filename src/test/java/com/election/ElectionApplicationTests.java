23872178a6b11f6f6629d6d75035388d00c51601
sravyasahithi <100826199+sravyasahithi@users.noreply.github.com>
2022-05-04 23:01:22 +0530

    Update ElectionApplicationTests.java

--- src/test/java/com/election/ElectionApplicationTests.java
+++ src/test/java/com/election/ElectionApplicationTests.java
@@ -3,7 +3,7 @@ package com.election;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ElectionApplication.class)
 class ElectionApplicationTests {
 
 	@Test
